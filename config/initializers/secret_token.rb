a8aa0c57f7c5e6b6283e84dc44f758590de0470a
giapiazze <giapiazze@gmail.com>
2013-11-18 16:24:47 +0100

    2013 11 18 - Inizio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prova::Application.config.secret_key_base = '6830d65fc2113f1409c4f9af6f28e697e77cf62329af0bb05ff9a4a8b894fa62657297ea799c411ec2173c3c763ed8b9658e39c55c493b665cd0a8432d3a3a15'
